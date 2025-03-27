060a0afd26fe19e683366eb35d2d977358aca911
Thakshila Fonseka <thakshilafonseka2002@gmail.com>
2025-03-27 10:02:20 +0530

    Separating the App widget from the main.dart file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ecommerce_app_shopfit/main.dart';
+import 'package:ecommerce_app_shopfit/app.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
